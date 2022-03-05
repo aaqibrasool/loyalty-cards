a1f59d446c6eac24bfdb46f571863b292423401b
aaqibrasool <aqibrasool10.com>
2022-03-05 16:24:44 +0500

    making the card details page functional

--- src/pages/CardDetails.tsx
+++ src/pages/CardDetails.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import useSWR from "swr";
+import axios from "axios";
+
+import CircularProgress from "@mui/material/CircularProgress";
+import Typography from "@mui/material/Typography";
+import { allCardsUrl } from "../constants/externalEndPoints";
+import CardInfo from "../components/CardInfo";
+import { CardType } from "../types/Card";
+
+const fetchFirstCard = (url: string) =>
+  axios.get(url).then((res) => res.data[0] as CardType);
 
 const CardDetails = () => {
-  return (
-    <div>CardDetails</div>
-  )
-}
+  const { data, error } = useSWR(allCardsUrl, fetchFirstCard);
 
-export default CardDetails
+  if (error) {
+    return <Typography variant="h3">Error while loading card</Typography>;
+  }
+
+  return (
+    <>
+      {data ? (
+        <CardInfo 
+        {...data}
+        />
+      ) : (
+        <CircularProgress />
+      )}
+    </>
+  );
+};
+
+export default CardDetails;
